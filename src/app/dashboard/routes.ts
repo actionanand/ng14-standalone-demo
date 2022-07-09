c3c35f04415e56a6d9e8c53643d059abe6c092ff
AnandRaja-tech <anand.official.in@gmail.com>
2022-07-09 13:43:28 +0000

    completed

--- src/app/dashboard/routes.ts
+++ src/app/dashboard/routes.ts
@@ -6,10 +6,12 @@ import { DashboardComponent } from './dashboard.component';
 export const DASHBOARD_ROUTES :Route[] = [
   {
     path: '',
-    component: DashboardComponent
+    component: DashboardComponent,
+    title: 'Dashboard'
   },
   {
     path: 'form',
-    loadComponent: () => import('./typed-form/typed-form.component').then(mod => mod.TypedFormComponent)
+    loadComponent: () => import('./typed-form/typed-form.component').then(mod => mod.TypedFormComponent),
+    title: 'Typed Form'
   }
 ];
